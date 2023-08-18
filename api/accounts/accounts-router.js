03576d91a34f4a9f423b2f1e6ebfd343d7412870
Kenneth Fisher <kenandcrys@live.com>
2023-08-18 09:04:01 -0500

    wired endpoints

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,55 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR 
+  try{
+      res.json([{},{}])
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR 
+  try{
+    throw new Error("This is router.get `/:id`")
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR 
+  try{
+    throw new Error("This is router.post `/`")
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR 
+  try{
+    throw new Error("This is router.put `/:id`")
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR 
+  try{
+    throw new Error("This is router.delete `/:id`")
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  // DO YOUR 
+  res.status(err.status || 500).json({
+    message: err.message,
+  })
 })
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
 
+const accRoutes = require('./accounts/accounts-router')
+
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accRoutes)
+
+
+server.use('*', (_req, res) => {
+    res.status(404).json({
+        message: "not found"
+    });
+});
+
 
 module.exports = server;
